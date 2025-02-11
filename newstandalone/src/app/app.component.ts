65d971915f23d1043bb7943c1241a48e373d60be
zss0527 <zss0527@outlook.com>
2025-02-11 22:03:45 +0800

    material ui, components, interpolation, pipes

--- newstandalone/src/app/app.component.ts
+++ newstandalone/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from "./home/home.component";
+import { CommonModule } from '@angular/common';
+import { ReversePipe } from './custom/reverse.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule, HomeComponent, CommonModule, ReversePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'newstandalone';
+  title = 'Agular 18 Tutorial';
+  subtitle = 'Angular for begginers'
+  todaydate = new Date()
+  salary = 1000
+  _obj = {
+    "name": "INT"
+  }
 }
