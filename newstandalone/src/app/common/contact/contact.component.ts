09edd69f28e12c6b00fb847b57e836530becda82
zss0527 <zss0527@outlook.com>
2025-02-12 22:20:33 +0800

    angular hooks

--- newstandalone/src/app/common/contact/contact.component.ts
+++ newstandalone/src/app/common/contact/contact.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   template: `
     <p>
       contact works!
+      <input [(ngModel)]='content'/>{{content}}
     </p>
   `,
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+  constructor() {
+    console.log('constructor...')
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked....')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit....')
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked...')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit....')
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck....')
+  }
+  content = ''
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges:', changes)
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit!')
+  }
+
 
 }
